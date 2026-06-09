24a59ea050addf419044968cc7e75837f3360a8b
bvoyage812-ui <bvoyage812@gmail.com>
2026-06-09 20:02:10 +0800

    Update script.js

--- Churro/script.js
+++ Churro/script.js
@@ -4,14 +4,14 @@ const btnContainer = document.getElementById('btnContainer');
 
 document.getElementById('yesBtn').addEventListener('click', function() {
     question.innerText = "That is great to hear! I am glad to hear that, Churrito. <3";
-    gif.src = "catt.gif";
+    gif.src = "images/catt.gif";
     document.body.style.backgroundColor = "#ffafcc";
     
     btnContainer.innerHTML = '<button id="tyBtn">Thank you!</button>';
     
     document.getElementById('tyBtn').addEventListener('click', function() {
         question.innerText = "You are always welcome churritoo, I will always be here for you! <3";
-        gif.src = "cutebunny.gif";
+        gif.src = "images/cutebunny.gif";
         document.body.style.backgroundColor = "#ffb5d0";
         
         btnContainer.innerHTML = '<button onclick="location.reload()">Return</button>';
